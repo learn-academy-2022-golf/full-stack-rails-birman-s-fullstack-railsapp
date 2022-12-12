d64e1e795a135ea2b749de0d6a43430479b7ea53
Charlie19255 <charlie19255@gmail.com>
2022-12-12 11:41:16 -0800

    Second story complete

--- app/controllers/post_blog_controller.rb
+++ app/controllers/post_blog_controller.rb
@@ -3,6 +3,9 @@ class PostBlogController < ApplicationController
     def index
         @blogs =Blog.all
     end
+    def show
+        @blogs = Blog.find(params[:id])
+    end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 root 'post_blog#index'
 
 get 'blogs' => 'post_blog#index', as: 'blogs'
-#  get 'blogs/:id' => 'post_blog#show', as : 'blog'
+ get 'blogs/:id' => 'post_blog#show', as: 'blog'
 end
 
 
-# Blog.create title:"Top 5 Destinations to Visit" , content:"
+#  Blog.create title:"Top 5 Destinations to Visit", cont
+# ent:"Phillipines, Korea, Maldives, Austrailia, Spain"
